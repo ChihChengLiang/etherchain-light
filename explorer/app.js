3f111e7bc610c5ca9ddd7f2360902d6a428b5b80
Chih Cheng Liang <chihchengliang@gmail.com>
2018-01-01 13:37:48 +0800

    remove contract verification

--- explorer/app.js
+++ explorer/app.js
@@ -10,7 +10,6 @@ var block = require('./routes/block');
 var tx = require('./routes/tx');
 var account = require('./routes/account');
 var accounts = require('./routes/accounts');
-var contract = require('./routes/contract');
 var signature = require('./routes/signature');
 var search = require('./routes/search');
 
@@ -50,7 +49,6 @@ app.use('/block', block);
 app.use('/tx', tx);
 app.use('/account', account);
 app.use('/accounts', accounts);
-app.use('/contract', contract);
 app.use('/signature', signature);
 app.use('/search', search);
 
